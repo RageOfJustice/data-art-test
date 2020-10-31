dfa0a75bba23c32dd547bf5f3b2c14e4edd12fdb
Nikolay Vovk <awhiffofzephyr@yandex.ru>
2020-10-31 20:40:29 +0300

    Add firebase database

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,69 @@
-import React from 'react';
-import { List, Container, CssBaseline } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { List, Container, CssBaseline, Grid } from '@material-ui/core';
 import { ListItem } from './components';
 
-const data = [
-  {
-    id: '1',
-    text: 'Hello',
-    done: false,
-  },
-  {
-    id: '2',
-    text: 'Duck',
-    done: true,
-  },
-];
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const db = firebase.database();
+// const ref = db.ref('lists').push();
+// ref.set({
+//   name: 'test',
+//   id: ref.key,
+//   items: [
+//     {
+//       id: '1',
+//       text: 'Hello',
+//       done: false,
+//     },
+//     {
+//       id: '2',
+//       text: 'Duck',
+//       done: true,
+//     },
+//   ],
+// });
+
+interface TODOItem {
+  text: string;
+  done?: boolean;
+  id: string;
+}
+
+interface TODOList {
+  id: string;
+  name: string;
+  items: TODOItem[];
+}
 
 function App() {
+  const [lists, setLists] = useState<TODOList[]>([]);
+
+  useEffect(() => {
+    db.ref('lists').on('value', (snapshot) => {
+      if (snapshot.exists()) {
+        setLists(Object.values(snapshot.val()) as TODOList[]);
+      } else {
+        setLists([]);
+      }
+    });
+  }, []);
   return (
     <Container>
       <CssBaseline />
-      <List>
-        {data.map(({ text, done, id }, index) => (
-          <ListItem done={done} key={id}>
-            {text}
-          </ListItem>
+      <Grid container spacing={2}>
+        {lists.map(({ items, id }) => (
+          <Grid key={id} item xs={12} md={6} lg={3}>
+            <List>
+              {items.map(({ text, done, id }) => (
+                <ListItem done={done} key={id}>
+                  {text}
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
         ))}
-      </List>
+      </Grid>
     </Container>
   );
 }
---
+++ src/init-firebase.ts
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCzZ-jDA-ZW8L0ULtx7u0q1axJ2bTYQgYk',
+  authDomain: 'data-art-test.firebaseapp.com',
+  databaseURL: 'https://data-art-test.firebaseio.com',
+  projectId: 'data-art-test',
+  storageBucket: 'data-art-test.appspot.com',
+  messagingSenderId: '117390352038',
+  appId: '1:117390352038:web:19f5fa18939cea06105e67',
+});
